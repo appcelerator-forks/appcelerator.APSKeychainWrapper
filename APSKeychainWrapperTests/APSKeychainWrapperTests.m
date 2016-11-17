8c0f381b12f1ab4be76ae83603022fa9e437650d
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2016-11-17 08:10:52 -0800

    Add test-target

---
+++ APSKeychainWrapperTests/APSKeychainWrapperTests.m
@@ -0,0 +1,26 @@
+/**
+ * APSKeychainWrapperTests
+ * Copyright (c) 2009-2016 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+#import <XCTest/XCTest.h>
+#import "APSKeychainWrapper.h"
+
+@interface APSKeychainWrapperTests : XCTestCase
+
+@end
+
+@implementation APSKeychainWrapperTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+@end
