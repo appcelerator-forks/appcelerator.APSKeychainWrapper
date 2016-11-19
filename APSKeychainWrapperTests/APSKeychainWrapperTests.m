43a98faeeef55e3475f831901dbc8cbb62b01849
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2016-11-19 08:05:14 -0800

    Move to distributable static library

--- APSKeychainWrapperTests/APSKeychainWrapperTests.m
+++ APSKeychainWrapperTests/APSKeychainWrapperTests.m
@@ -1,5 +1,5 @@
 /**
- * APSKeychainWrapperTests
+ * APSKeychainWrapper
  * Copyright (c) 2016 by Axway. All Rights Reserved.
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
@@ -7,20 +7,124 @@
 #import <XCTest/XCTest.h>
 #import "APSKeychainWrapper.h"
 
-@interface APSKeychainWrapperTests : XCTestCase
+NSString * const kAPSKeychainWrapperIdentifier = @"password";
+NSString * const kAPSKeychainWrapperService = @"ti.touchid";
+NSString * const kAPSKeychainWrapperAccessGroup = @"com.appcelerator.keychainwrapper";
 
+@interface APSKeychainWrapperTests : XCTestCase <APSKeychainWrapperDelegate>
+@property(nonatomic, strong, readwrite) APSKeychainWrapper *keychainItem;
 @end
 
 @implementation APSKeychainWrapperTests
 
-- (void)setUp {
+- (void)setUp
+{
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    self.keychainItem = [[APSKeychainWrapper alloc] initWithIdentifier:kAPSKeychainWrapperIdentifier
+                                                               service:kAPSKeychainWrapperService
+                                                           accessGroup:kAPSKeychainWrapperAccessGroup];
+    
+    self.keychainItem.delegate = self;
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+- (void)tearDown
+{
+    self.keychainItem.delegate = nil;
+    
     [super tearDown];
+}
+
+- (void)testSaveToKeychain
+{
+    XCTAssertNotNil(self.keychainItem, @"Keychain item is nil");
+    XCTAssertTrue([self.keychainItem respondsToSelector:@selector(save:)], @"Selector save: not found");
+    
+    [self.keychainItem save:@"secret_password"];
+}
+
+- (void)testReadFromKeychain
+{
+    XCTAssertNotNil(self.keychainItem, @"Keychain item is nil");
+    XCTAssertTrue([self.keychainItem respondsToSelector:@selector(read)], @"Selector save: not found");
+    
+    [self.keychainItem read];
+}
+
+- (void)testUpdateToKeychain
+{
+    XCTAssertNotNil(self.keychainItem, @"Keychain item is nil");
+    XCTAssertTrue([self.keychainItem respondsToSelector:@selector(update:)], @"Selector save: not found");
+    
+    [self.keychainItem update:@"new password"];
+}
+
+- (void)testDeleteFromKeychain
+{
+    XCTAssertNotNil(self.keychainItem, @"Keychain item is nil");
+    XCTAssertTrue([self.keychainItem respondsToSelector:@selector(reset)], @"Selector save: not found");
+    
+    [self.keychainItem reset];
+}
+
+- (void)APSKeychainWrapper:(APSKeychainWrapper *)keychainWrapper didSaveValueWithResult:(NSDictionary *)result
+{
+    XCTAssertNotNil(result, "Keychain item result is empty");
+    XCTAssertTrue([result isKindOfClass:[NSDictionary class]], "Result is not a dictionary");
+    XCTAssertEqual([[result allKeys] count], 1, @"Result is not length = 1");
+    
+    XCTAssertNotNil([result objectForKey:@"success"], "Keychain key \"success\" item result is nil");
+    XCTAssertTrue([result objectForKey:@"success"], "Keychain key \"success\" item result is not true");
+}
+
+- (void)APSKeychainWrapper:(APSKeychainWrapper *)keychainWrapper didSaveValueWithError:(NSError *)error
+{
+    XCTAssertNil(error, @"Error is non-nil");
+}
+
+- (void)APSKeychainWrapper:(APSKeychainWrapper*)keychainWrapper didUpdateValueWithResult:(NSDictionary*)result
+{
+    XCTAssertNotNil(result, "Keychain item result is empty");
+    XCTAssertTrue([result isKindOfClass:[NSDictionary class]], "Result is not a dictionary");
+    XCTAssertEqual([[result allKeys] count], 1, @"Result is not length = 1");
+
+    XCTAssertNotNil([result objectForKey:@"success"], "Keychain key \"success\" item result is nil");
+    XCTAssertTrue([result objectForKey:@"success"], "Keychain key \"success\" item result is not true");
+}
+
+- (void)APSKeychainWrapper:(APSKeychainWrapper*)keychainWrapper didUpdateValueWithError:(NSError*)error
+{
+    XCTAssertNil(error, @"Error is non-nil");
+}
+
+- (void)APSKeychainWrapper:(APSKeychainWrapper*)keychainWrapper didReadValueWithResult:(NSDictionary*)result
+{
+    XCTAssertNotNil(result, "Keychain item result is empty");
+    XCTAssertTrue([result isKindOfClass:[NSDictionary class]], "Result is not a dictionary");
+    XCTAssertEqual([[result allKeys] count], 1, @"Result is not length = 1");
+    
+    XCTAssertNotNil([result objectForKey:@"success"], "Keychain key \"success\" item result is nil");
+    XCTAssertTrue([result objectForKey:@"success"], "Keychain key \"success\" item result is not true");
+}
+
+- (void)APSKeychainWrapper:(APSKeychainWrapper*)keychainWrapper didReadValueWithError:(NSError*)error
+{
+    XCTAssertNil(error, @"Error is non-nil");
+}
+
+- (void)APSKeychainWrapper:(APSKeychainWrapper*)keychainWrapper didDeleteValueWithResult:(NSDictionary*)result
+{
+    XCTAssertNotNil(result, "Keychain item result is empty");
+    XCTAssertTrue([result isKindOfClass:[NSDictionary class]], "Result is not a dictionary");
+    XCTAssertEqual([[result allKeys] count], 1, @"Result is not length = 1");
+    
+    XCTAssertNotNil([result objectForKey:@"success"], "Keychain key \"success\" item result is nil");
+    XCTAssertTrue([result objectForKey:@"success"], "Keychain key \"success\" item result is not true");
+}
+
+- (void)APSKeychainWrapper:(APSKeychainWrapper*)keychainWrapper didDeleteValueWithError:(NSError*)error
+{
+    XCTAssertNil(error, @"Error is non-nil");
 }
 
 @end
